37ac9d7c96e145fca1c01dea075587b32c8f9cba
mao <1296193245@qq.com>
2023-12-26 16:33:31 +0800

    uniapp-media-image

---
+++ pages/index/test1/test1.vue
@@ -0,0 +1,32 @@
+<template>
+	<view>
+		<button  type="primary" @click="button1">选择图片</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			button1()
+			{
+				uni.chooseImage({
+					count: 8,
+					sizeType: ['original', 'compressed'],
+					sourceType: ['album'], //从相册选择
+					success: function (res) {
+						console.log(JSON.stringify(res.tempFilePaths));
+					}
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index/test2/test2.vue
@@ -0,0 +1,43 @@
+<template>
+	<view>
+		<button type="primary" @click="button1">预览图片</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+			button1() {
+				uni.chooseImage({
+					count: 8,
+					sizeType: ['original', 'compressed'],
+					sourceType: ['album'], //从相册选择
+					success: function(res) {
+						uni.previewImage({
+							urls: res.tempFilePaths,
+							longPressActions: {
+								itemList: ['发送给朋友', '保存图片', '收藏'],
+								success: function(data) {
+									console.log('选中了第' + (data.tapIndex + 1) + '个按钮,第' + (data
+										.index + 1) + '张图片');
+								},
+								fail: function(err) {
+									console.log(err.errMsg);
+								}
+							}
+						});
+					}
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
