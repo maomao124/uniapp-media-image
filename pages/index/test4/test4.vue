b71bdd6ff36201238f351ec4dad3b2e4a834e6a9
mao <1296193245@qq.com>
2023-12-26 16:55:28 +0800

    uniapp-media-image

---
+++ pages/index/test4/test4.vue
@@ -0,0 +1,46 @@
+<template>
+	<view>
+		<button type="primary" @click="button1">查看本地图片信息</button>
+		<button type="primary" @click="button2">查看网络图片信息</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+			button1() {
+				uni.getImageInfo({
+					src:'/static/logo.png',
+					success: (data) => {
+						console.log(data);
+					},
+					fail: (err) => {
+						console.log("错误",err);
+					}
+				})
+			},
+			button2() {
+				uni.getImageInfo({
+					src:'https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/uni-app.png',
+					success: (data) => {
+						console.log(data);
+					},
+					fail: (err) => {
+						console.log("错误",err);
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+button{
+	margin: 5px;
+}
+</style>
---
+++ pages/index/test5/test5.vue
@@ -0,0 +1,40 @@
+<template>
+	<view>
+		<button type="primary" @click="button1">选择图片保存</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+			button1() {
+				uni.chooseImage({
+					count: 1,
+					sizeType: ['original', 'compressed'],
+					sourceType: ['album'], //从相册选择
+					success: function(res) {
+
+						uni.saveImageToPhotosAlbum({
+							filePath: res.tempFilePaths[0],
+							success: (data) => {
+								console.log(data);
+							},
+							fail: (err) => {
+								console.log("错误：", err);
+							}
+						})
+					}
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
